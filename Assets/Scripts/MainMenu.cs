5da93a451c00fdbc0416cf603a559b70b7e39656
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-02 02:47:21 -0500

    Add tasks 8 and 9: enable game start from main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/// <summary>Contains event handlers for the main menu.</summary>
+public class MainMenu : MonoBehaviour
+{
+    public void PlayMaze() {
+        SceneManager.LoadScene("maze");
+    }
+}
